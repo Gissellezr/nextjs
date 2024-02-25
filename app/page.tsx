8359c055f078b572575fec47f1a31afd26096e05
Marcelo <marcelo95326@icloud.com>
2024-02-25 13:57:02 -0500

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,40 @@
 import Image from "next/image";
 import Logo from "../public/logo.png";
+import React from "react";
 
 export default function Home() {
+ 
+ 
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <div className="relative flex flex-col items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <div className="relative">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src={Logo}
-            alt="Logo"
-            width={320}
-            height={64}
-            priority
-          />
+      <section id="landing">
+        <div className="relative flex flex-col items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          <div className="relative">
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src={Logo}
+              alt="Logo"
+              width={480}
+              height={96}
+              priority
+            />
+          </div>
+          {/* Text */}
+          <div className="mt-[-140px] transform translate-x-20 z-10">
+            <h1 className="text-3xl font-light text-gray-500">
+              Visual Designer
+            </h1>
+          </div>
         </div>
-        {/* Text */}
-        <div className="mt-[-80px] transform translate-x-20  z-10">
-          <h1 className="text-xl font-bold text-gray-500">
-            Visual Designer.
-          </h1>
-        </div> 
-      </div> 
-   
-      {/* Buttons */}
-      <div className="flex justify-center mt-6">
-        <button className="px-4 py-2 mr-2 rounded-md">Work</button>
-        <button className="px-4 py-2 ml-2 rounded-md">About</button>
-        <button className="px-4 py-2 ml-2 rounded-md">Contact</button>
-      </div>
+
+        {/* Buttons */}
+        <div className="flex justify-center mt-16">
+          <button className="px-4 py-2 mr-4 rounded-md text-xl font-light hover:font-semibold">About</button>
+          <button className="px-4 py-2 mx-40 rounded-md text-xl font-light hover:font-semibold">Work</button>
+          <button className="px-4 py-2 ml-4 rounded-md text-xl font-light hover:font-semibold">Contact</button>
+        </div>
+      </section>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,27 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      spacing: {
+        '-100': '-100px',
+        '-120': '-120px',
+        '-140': '-140px',
+       
+      },
+      translate: {
+        '-20.5': '-20.5px',
+        '-21': '-21px',
+        '-21.5': '-21.5px',
+        '-22': '-22px',
+        '-22.5': '-22.5px',
+        '-23': '-23px',
+        '-23.5': '-23.5px',
+        '-24': '-24px',
+        '-24.5': '-24.5px',
+        '-25': '-25px',
+
+        
+        // Add as many as you need
+      }
     },
   },
   plugins: [],
